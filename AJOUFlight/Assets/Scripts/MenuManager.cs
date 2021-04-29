4142c92697b6e7132063b6ec44aca372f569831b
InYongShin <tlsdlsdyd98@gmail.com>
2021-04-29 23:28:45 +0900

    Basic MenuScene

---
+++ AJOUFlight/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene("PlayScene");
+    }
+
+    public void OnClickRoom()
+    {
+        SceneManager.LoadScene("RoomScene");
+    }
+
+    public void OnClickRanking()
+    {
+        SceneManager.LoadScene("RankingScene");
+    }
+
+    public void OnClickStore()
+    {
+        SceneManager.LoadScene("StoreScene");
+    }
+
+    public void OnClickExit()
+    {
+        Application.Quit();
+    }
+}
---
+++ AJOUFlight/Assets/Scripts/StoreManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoreManager : MonoBehaviour
+{
+
+    public void OnClickExit()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
